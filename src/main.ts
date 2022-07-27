2a8189c7c27c745b74890590bf1d6f859fafd38e
Benjamin Darbonne <user@nomail.com>
2022-07-27 11:28:55 -0500

    Fixes missing notification on linux

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,32 @@
 import {notifier} from 'node-notifier'
 import path from 'path'
+import os from 'os'
+import notifySend from 'notify-send'
+
 
 type Notification = {
   title:string,
   message:string
 }
 
+const iconPath = path.join(__dirname, "../img/archLogo.png")
+
 const notify = (notification: Notification) => {
-  notifier.notify({
-    title: notification.title,
-    message: notification.message,
-    icon: path.join(__dirname, "../img/archLogo.png")
-  })
+  switch(os.type()){
+    case 'Linux':
+      notifySend.icon(iconPath)
+                .notify(notification.title, notification.message)
+      break;
+    default:
+      notifier.notify({
+        title: notification.title,
+        message: notification.message,
+        icon: iconPath
+      })
+      break;
+  }
 }
+
 
 export type { Notification }
 export { notify }
